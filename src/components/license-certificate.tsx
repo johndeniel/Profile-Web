c08bba3e9c8a342dcab51923537422d47a4b1217
johndeniel <johndeniel.d37@gmail.com>
2026-09-10 21:29:53 +0800

    Standardize section headers and remove unnecessary badges

--- src/components/license-certificate.tsx
+++ src/components/license-certificate.tsx
@@ -71,7 +71,7 @@ export function LicenseCertificateList({
   };
 
   return (
-    <div className="flex flex-col gap-4 pt-8">
+    <div className="flex flex-col gap-4">
       {/* Header */}
       <div className="flex items-center justify-between">
         <h3 className="font-heading text-xl font-bold tracking-tight text-foreground">
